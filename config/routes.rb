d7e7e1e7bd9127c6395e8c2c1aa62738dc568408
Gilbert Gotora <gilbertgotora2@gmail.com>
2021-04-02 18:22:53 +0200

    setup homepage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registratons#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
