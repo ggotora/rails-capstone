065a99d689e7196392cafa382fd60923cb70e498
Gilbert Gotora <gilbertgotora2@gmail.com>
2021-04-04 15:15:41 +0200

    add registration routes, controller and views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,22 @@
 class RegistrationsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "New Account Created"
+      session[:user_id] = @user.id
+      redirect_to root_path 
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :full_name)
   end
 end
