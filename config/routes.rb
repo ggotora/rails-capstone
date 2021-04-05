2ffd962fffa6cba0740f4fede1e164bb93920f91
Gilbert Gotora <gilbertgotora2@gmail.com>
2021-04-05 11:16:28 +0200

    Add sign_in and logout session views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   root 'home#index'
   # get 'sign_up', to: 'registrations#new'
   # post 'sign_up', to: 'registrations#create'
   resources :users
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  
+  delete 'logout', to: 'sessions#destroy'
 
   # get 'edit_user', to: 'users#edit'
   # patch 'edit_user', to: 'users#update'
